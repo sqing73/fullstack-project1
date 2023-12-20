15c908de2fc0819063d5e5acbfe62dd1e4a669a8
rui221 <72562873+rui221@users.noreply.github.com>
2023-12-20 14:46:03 -0800

    update cart for edit and checkout
    for cart can add and remove.
    for checkout, check stock first, then if the stock > demand, show successful, then the stock-demand

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -9,11 +9,21 @@ exports.getCart = async (req, res) => {
         if (!cart) {
             return res.status(404).json({ message: 'Cart not found' });
         }
-        res.json(cart);
+
+        // Calculate total price
+        let totalPrice = 0;
+        cart.items.forEach(item => {
+            totalPrice += item.quantity * item.product.price;
+        });
+
+        // Respond with the cart and total price
+        res.json({ cart: cart, totalPrice: totalPrice });
     } catch (error) {
         res.status(500).json({ message: 'Error retrieving cart', error: error.message });
     }
 };
+
+
 
 // Update the cart (add/remove items, change quantity)
 exports.updateCart = async (req, res) => {
@@ -21,15 +31,8 @@ exports.updateCart = async (req, res) => {
         const userId = req.user._id;
         const { productId, quantity } = req.body;
 
-        // Debug logs
-        console.log('updateCart - User ID:', userId);
-        console.log('updateCart - Received Product ID:', productId);
-        console.log('updateCart - Received Quantity:', quantity);
-
         // Validate the product
         const product = await Product.findById(productId);
-        console.log('updateCart - Product Lookup Result:', product);
-
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
@@ -40,40 +43,58 @@ exports.updateCart = async (req, res) => {
             cart = new Cart({ user: userId, items: [] });
         }
 
-        // Find the item in the cart
         const itemIndex = cart.items.findIndex(item => item.product.toString() === productId);
-        console.log('updateCart - Item Index in Cart:', itemIndex);
 
-        if (itemIndex > -1) {
-            // Update item quantity if it already exists
-            cart.items[itemIndex].quantity += quantity;
-        } else {
-            // Add new item to the cart
-            cart.items.push({ product: productId, quantity: quantity });
+        if (quantity > 0) {
+            if (itemIndex > -1) {
+                // Update item quantity
+                cart.items[itemIndex].quantity = quantity;
+            } else {
+                // Add new item to the cart
+                cart.items.push({ product: productId, quantity });
+            }
+        } else if (itemIndex > -1) {
+            // Remove item from the cart
+            cart.items.splice(itemIndex, 1);
         }
 
         // Save the updated or new cart
         await cart.save();
         res.status(200).json(cart);
     } catch (error) {
-        console.error('updateCart - Error:', error);
         res.status(500).json({ message: 'Error updating cart', error: error.message });
     }
 };
+
 
 // Handle the checkout process
 exports.checkout = async (req, res) => {
     try {
         const userId = req.user._id;
-        const cart = await Cart.findOne({ user: userId });
+        const cart = await Cart.findOne({ user: userId }).populate('items.product');
+
         if (!cart || cart.items.length === 0) {
             return res.status(400).json({ message: 'Cart is empty' });
         }
 
-        // Logic for the checkout process
-        // This could include checking stock, processing payment, etc.
+        // Check stock for each item in the cart
+        for (const item of cart.items) {
+            if (item.quantity > item.product.stock) {
+                return res.status(400).json({ message: `${item.product.name} is out of stock` });
+            }
+        }
 
-        // After successful checkout, you might want to clear the cart
+        // Here, add logic to process the payment
+        // For example, integrate with a payment gateway API
+
+        // Update the stock for each product
+        for (const item of cart.items) {
+            const product = await Product.findById(item.product._id);
+            product.stock -= item.quantity; // Deduct the quantity from the product's stock
+            await product.save();
+        }
+
+        // Clear the cart after successful checkout
         cart.items = [];
         await cart.save();
 
